0302d869a56089e01ddc16728f70b7f9adc63333
Digvijay <sawaledigvijay70@gmail.com>
2025-11-26 19:54:44 +0530

    add create and view article function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,22 @@ class ArticlesController < ApplicationController
   def index 
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    # byebug
+    # render plain: params[:article].inspect
+
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      # redirect_to articles_path
+      flash[:notice] = "Article was created successfully"
+      redirect_to articles_path(@article)
+    else
+      render 'new'
+    end
+  end
 end
