fee8284bfad6ba17675ba1c59e82d83b8ed09fce
Digvijay <sawaledigvijay70@gmail.com>
2025-12-04 20:18:04 +0530

    update show and index view for user article association

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_04_105627) do
+ActiveRecord::Schema[8.1].define(version: 2025_12_04_121109) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
