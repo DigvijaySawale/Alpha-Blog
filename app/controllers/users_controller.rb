f230d4fa1a3ad70be5e94c7e3fe89a1298d36931
Digvijay <sawaledigvijay70@gmail.com>
2025-12-06 20:49:58 +0530

    add auth helper methods and modify nav partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:edit, :update, :show]
+  
   def index 
     # @users = User.all
     @users = User.paginate(page: params[:page], per_page: 2)
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha blog #{@user.username}, you have signed up successfully"
       redirect_to articles_path
     else
@@ -21,12 +23,12 @@ class UsersController < ApplicationController
   end
 
   def edit 
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])   ##added to before action
   end
 
   def update
     # binding.pry
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])   ##added to before action
     if @user.update(user_params)
       flash[:notice] = "User Details was updated successfully"
       redirect_to @user
@@ -36,7 +38,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    #@user = User.find(params[:id])   ##added to before action
     # @articles = @user.articles
     @articles = @user.articles.paginate(page: params[:page], per_page: 2)
 
@@ -45,5 +47,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
